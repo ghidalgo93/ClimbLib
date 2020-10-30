c301620cd1de5bff85e84d226a4c6297bf2c834c
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2020-10-30 15:20:12 -0600

    add dom manipulations: createTable, createTableBody

--- src/main.js
+++ src/main.js
@@ -41,30 +41,55 @@ ClimbLib.prototype.notSend = function(index){
 // **************************************
 function DomManipulation(){}
 
-DomManipulation.prototype.init = function(){
-	const form = document.createElement('form');
-	const table = document.createElement('table');
-	form.id = 'addClimbForm';
+DomManipulation.prototype.createTable = function(headings){
+	let table = document.createElement('table');
 	table.id = 'climbTable';
-	return {
-		form, table
+	let thead = table.createTHead(); 	
+	let row = thead.insertRow();
+	for (let key of headings){
+		let thCell = document.createElement('th');
+		let text = document.createTextNode(key);
+		thCell.appendChild(text);
+		row.appendChild(thCell);
 	}
+	return table;
 }
 
-DomManipulation.prototype.createInput = function(name='', placeholder='', type='text', value='', maxlength='200', required='required'){
-	const input = document.createElement('input');
-	input.name = name;
-	input.placeholder = placeholder;
-	input.type = type;
-	input.value = value;
-	input.maxLength = maxlength;
-	input.required = required;
-	return input;
-}
+DomManipulation.prototype.createTableBody = function(climbLib){
+	let tbody = document.createElement('tbody');
+	let lib = climbLib.getLib();
+	for (let climb of lib){
+		let row = tbody.insertRow();
+		let values = Object.values(climb);
+		for (let value of values){
+			let tdCell = document.createElement('td');
+			let text = document.createTextNode(value);
+			tdCell.appendChild(text);
+			row.appendChild(tdCell);
+		}
+	}
 
+	return tbody;
+}
 
 
 //*******SCRIPT*********//
+const ogClimb = new Climb();
+const headings = Object.keys(ogClimb);
+
+const climbLib = new ClimbLib();
+const climb1 = new Climb('test.name1', 'test.grade1', 'test.location1', 'test.date1', 'test.type1', false);
+const climb2 = new Climb('test.name2', 'test.grade2', 'test.location2', 'test.date2', 'test.type2', true);
+climbLib.addClimb(climb1);
+climbLib.addClimb(climb2);
+
+const body = document.querySelector('body');
+const Dom = new DomManipulation();
+const table = Dom.createTable(headings);
+body.appendChild(table);
+table.appendChild(Dom.createTableBody(climbLib));
+
+
 
 module.exports.Climb = Climb;
 module.exports.ClimbLib = ClimbLib;
